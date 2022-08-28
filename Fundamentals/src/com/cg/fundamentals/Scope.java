be573d219bc716f1888e56fad78b7f390f20ba00
Win <Win@Dell>
2022-08-28 21:27:16 +0530

    first commit

---
+++ Fundamentals/src/com/cg/fundamentals/Scope.java
@@ -0,0 +1,19 @@
+package com.cg.fundamentals;
+
+public class Scope {
+
+	public static void main(String[] args) {
+	
+		int outer = 1;
+		{
+			int inner = 2;
+			System.out.println("inner = " + inner);
+			System.out.println("outer = " + outer);
+		}
+		int inner = 3;
+		System.out.println("inner =" + inner);
+		System.out.println("outer =" + outer);
+
+	}
+
+}
